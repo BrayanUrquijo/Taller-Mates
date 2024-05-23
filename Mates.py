cec1ef6f6eaa3d129ba430237d190d64bfea6e5a
Brayan Urquijo <brayan.urquijo@correounivalle.edu.co>
2024-05-23 18:17:04 -0500

    Agregamos Nombre y Ventana

--- Mates.py
+++ Mates.py
@@ -7,3 +7,19 @@ pg.init()
 #creamos la pantalla
 
 pantalla = pg.display.set_mode((800, 600))
+
+#titulo de la ventana
+
+pg.display.set_caption("The God of Math")
+
+#bucle principal
+
+run = True
+
+while run == True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+pg.quit() #cerramos pygame
+
+
